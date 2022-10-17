c4dd9b42ef8125304779581df8aa93bc593173c7
freetimezp <kraftukrnet@ukr.net>
2022-10-17 15:31:37 +0300

    04. js-social-05-imagine. Login page use React-oauth/google.

---
+++ imagine_frontend/src/container/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='underline text-3xl'>
+            Home 123123
+        </div>
+    )
+}
+
+export default Home;
